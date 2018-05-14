f729f64ede7a67fcb98fab011413786078b7e1a6
lunannan <lunannan@crop.the9.com>
2018-05-14 17:36:25 +0800

    卢楠楠2018-05-14学习

---
+++ todolist/src/Todolist.vue
@@ -0,0 +1,45 @@
+<template>
+	<div>
+		<div>
+			<input v-model="inputValue" type="text" />
+			<button @click="handleSubmit">提交</button>
+		</div>
+		<ul class="item">
+			<todo-item
+				v-for="item, index in list" 
+				:key="index" 
+				:content="item" 
+				:index="index"
+				@delete="handleDelete"
+			></todo-item>
+		</ul>
+	</div>
+</template>
+
+<script>
+import TodoItem from "@/components/TodoItem";
+export default {
+    data() {
+        return {
+            inputValue: "",
+            list: []
+        };
+    },
+    methods: {
+        handleSubmit() {
+            this.list.push(this.inputValue);
+            this.inputValue = "";
+		},
+		handleDelete (index) {
+			this.list.splice(index, 1);
+		}
+    },
+    components: {
+        "todo-item": TodoItem
+    }
+};
+</script>
+
+<style>
+
+</style>
---
+++ todolist/src/components/TodoItem.vue
@@ -0,0 +1,22 @@
+<!--  -->
+<template>
+	<div>
+		<li class="item" @click="handleDelete">{{content}}</li>
+	</div>
+</template>
+
+<script>
+export default {
+    props: ["content", "index"],
+    methods: {
+        handleDelete() {
+            this.$emit("delete", this.index);
+        }
+    }
+};
+</script>
+<style scoped>
+.item{
+	color: red;
+}
+</style>
